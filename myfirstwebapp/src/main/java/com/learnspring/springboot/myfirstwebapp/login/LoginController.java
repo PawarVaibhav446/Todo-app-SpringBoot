6a63ed3941ef61cda9b1a5f62842602dcacb3f1c
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-21 11:58:29 +0530

    Logging

--- myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/login/LoginController.java
@@ -1,18 +1,24 @@
 package com.learnspring.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class LoginController {
 	
-	// http://localhost:8080/login-jsp?name=Vaibhav
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String login(@RequestParam String name, ModelMap model) {
+//		logger.debug("Debug: Request Param is {}" + name);
+//		logger.info("Info: Request Param is {}" + name);
+//		logger.warn("Info: Request Param is {}" + name);
 		model.put("name", name);
-		System.out.println("Request Param is " + name);
 		return "login";
 	}
 }
