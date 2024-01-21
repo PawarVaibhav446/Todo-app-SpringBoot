7fe636c71afcfd2c7b7f5cea5a7052d46b5bd43b
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-21 12:06:14 +0530

    Removed Logging! It was tough to understand.

--- myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/login/LoginController.java
@@ -1,7 +1,5 @@
 package com.learnspring.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
 	@RequestMapping("login")
 	public String login(@RequestParam String name, ModelMap model) {
-//		logger.debug("Debug: Request Param is {}" + name);
-//		logger.info("Info: Request Param is {}" + name);
-//		logger.warn("Info: Request Param is {}" + name);
 		model.put("name", name);
 		return "login";
 	}
