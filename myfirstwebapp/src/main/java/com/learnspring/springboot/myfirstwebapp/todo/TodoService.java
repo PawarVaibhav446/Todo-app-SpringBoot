1665f616c8a1da7f145cbb2c01edda7f90b9da25
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-22 20:25:12 +0530

    Created a Static List of Todos

---
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,21 @@
+package com.learnspring.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoService {
+	
+	private static List<Todo> todos;
+	static {
+		todos.add(new Todo(1, "Sleep", "Sleep for 8 hours", 
+				LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(1, "Eat", "Eat 3 times a Day", 
+				LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(1, "Study", "Study for 6 hours", 
+				LocalDate.now().plusYears(3), false));
+	}
+	
+	public List<Todo> findByUsername(String username){
+		return todos;
+	}
+}
