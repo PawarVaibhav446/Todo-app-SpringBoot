7dacbcf4dd3a19c5ef1631077b058b5c43391e1e
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-25 18:17:23 +0530

    Implemented addTodo Method in TodoService

--- myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,13 @@
 package com.learnspring.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -30,7 +32,9 @@ public class TodoController {
 	}
 	
 	@RequestMapping(value="add-todo", method = RequestMethod.POST)
-	public String addNewTodoPage() {
+	public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+		String username = (String) model.get("name");
+		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
--- myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoService.java
@@ -10,16 +10,24 @@ import org.springframework.stereotype.Service;
 public class TodoService {
 	
 	private static List<Todo> todos = new ArrayList<>();
+	private static int todosCount = 0;
+	
 	static {
-		todos.add(new Todo(1, "Sleep", "Sleep for 8 hours", 
+		todos.add(new Todo(++todosCount, "Sleep", "Sleep for 8 hours", 
 				LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "Eat", "Eat 3 times a Day", 
+		todos.add(new Todo(++todosCount, "Eat", "Eat 3 times a Day", 
 				LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(3, "Study", "Study for 6 hours", 
+		todos.add(new Todo(++todosCount, "Study", "Study for 6 hours", 
 				LocalDate.now().plusYears(3), false));
 	}
 	
 	public List<Todo> findByUsername(String username){
 		return todos;
+	}
+	
+	public void addTodo(String username, String description, 
+			LocalDate targetDate, boolean isDone) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, isDone);
+		todos.add(todo);
 	}
 }
