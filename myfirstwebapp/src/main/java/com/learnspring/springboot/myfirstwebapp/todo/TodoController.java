d357cc06bd10797cbb5044b3cdf2b28cbac8c6da
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-25 18:09:26 +0530

    Add New Todo

--- myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,22 +5,32 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TodoController {
-	
+
 	private TodoService todoService;
-	
+
 	public TodoController(TodoService todoService) {
 		super();
 		this.todoService = todoService;
 	}
 
-	//list-todos
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
 		List<Todo> todos = todoService.findByUsername("Sleep");
 		model.put("todos", todos);
 		return "listTodos";
+	}
+
+	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage() {
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method = RequestMethod.POST)
+	public String addNewTodoPage() {
+		return "redirect:list-todos";
 	}
 }
