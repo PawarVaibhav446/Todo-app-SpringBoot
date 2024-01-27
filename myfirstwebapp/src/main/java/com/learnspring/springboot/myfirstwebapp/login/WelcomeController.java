3e562838e50cc8ab42a61a99b74470b908b795fb
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-27 18:24:40 +0530

    Added Spring Security Configuration

--- myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/login/WelcomeController.java
+++ myfirstwebapp/src/main/java/com/learnspring/springboot/myfirstwebapp/login/WelcomeController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 
@@ -17,6 +16,4 @@ public class WelcomeController {
 		model.put("name", "Vaibhav");
 		return "welcome";
 	}
-
-
 }
